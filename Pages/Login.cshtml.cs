a6469aea4ac8733c328d01a857e5986364970892
Angelique BYUKUSENGE <mrsangelique003@gmail.com>
2026-08-25 08:49:26 +0200

    test

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Course_Schedule_Academic_Awareness_System_CSAAS.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Password { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Role { get; set; } = string.Empty;
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
